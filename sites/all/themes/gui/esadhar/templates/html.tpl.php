565ddafafef55bb730ea014cc9f674e929708d85
Bachir Soussi Chiadmi <bachir@g-u-i.me>
2015-01-19 15:40:05 +0100

    removed theme git submodule, keeping only one repos for the whole drupal

--- sites/all/themes/gui/esadhar/templates/html.tpl.php
+++ sites/all/themes/gui/esadhar/templates/html.tpl.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * @file
  * Default theme implementation to display the basic html structure of a single
@@ -55,42 +55,46 @@
 		<!-- remove the no-js as fast as possible -->
 		<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
 
-		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame 
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
 		     Remove this if you use the .htaccess -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 		<?php print $head; ?>
-		
+
 		<title><?php print $head_title; ?></title>
 		<?php print $styles; ?>
 
-	  
+
 	  <!-- All JavaScript at the bottom, except for Modernizr / Respond.
 	       Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries
 	       For optimal performance, use a custom Modernizr build: www.modernizr.com/download/ -->
-	  <script src="<?php print $theme_path ?>/js/libs/modernizr-2.6.1.min.js" type="text/javascript"></script>
+	  <script src="<?php print $theme_path ?>/bower_components/modernizr/modernizr.js" type="text/javascript"></script>
 
 
 		<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 	</head>
-	
+
 	<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
 	  <div id="skip-link">
 	    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
 	  </div>
 
-    <div id="container">		 		
-	
-		  <?php print $page_top; ?>
-		  <?php print $page; ?>
-		  <?php print $page_bottom; ?>
-	
-    </div> 
-    <!-- /container -->
+	  <?php print $page_top; ?>
+	  <?php print $page; ?>
+	  <?php print $page_bottom; ?>
 
-		<!-- Javascript at the bottom for fast page loading -->
-		<?php print $scripts; ?>
-		
+        <div id="main-bg-wrapper">
+          <div id="main-bg"
+                  data-smooth-scrolling="off"
+                  data-anchor-target="#center"
+                  data-top="width:100%; height:100%; margin-left:0%; margin-top:0%;"
+                  data-top-bottom="width:115%; height:115%; margin-left:-5%; margin-top:-7%;">
+          </div>
+        </div>
+
+        <!-- Javascript at the bottom for fast page loading -->
+        <?php print $scripts; ?>
+
 
 	  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
 	       chromium.org/developers/how-tos/chrome-frame-getting-started -->
@@ -100,5 +104,5 @@
 	  <![endif]-->
 
 	</body>
-	
+
 </html>
