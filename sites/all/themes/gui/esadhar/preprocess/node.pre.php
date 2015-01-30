e8686fc7797af8f545744452313257f247c69ce1
Bachir Soussi Chiadmi <bachir@g-u-i.me>
2015-01-30 10:48:24 +0100

    fixed php node.pre bug

--- sites/all/themes/gui/esadhar/preprocess/node.pre.php
+++ sites/all/themes/gui/esadhar/preprocess/node.pre.php
@@ -6,7 +6,7 @@ $vars['theme_hook_suggestions'][] = 'node__'.$vars['view_mode'];
 $vars['theme_hook_suggestions'][] = 'node__' . $vars['type'] . '__' . $vars['view_mode'];
 
 if(!isset($vars["title_attributes_array"]["class"]))
-  $vars["title_attributes_array"]["class"] = [];
+  $vars["title_attributes_array"]["class"] = array();
 
 $vars["title_attributes_array"]["class"][] = "node-title";
 $vars["title_attributes_array"]["class"] = implode(" ", $vars["title_attributes_array"]["class"]);
